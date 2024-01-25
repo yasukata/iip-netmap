9901847cfdb6863c8947751566d8669171245032
Kenichi Yasukata <kyasukata@outlook.com>
2024-01-25 10:21:31 +0900

    build.mk if section for Linux

--- build.mk
+++ build.mk
@@ -1,1 +1,8 @@
+ifeq ($(OSNAME),Linux)
+CD := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+NETMAP_DIR ?= $(CD)/netmap
+CFLAGS += -D_GNU_SOURCE
+CFLAGS += -I$(NETMAP_DIR)/libnetmap -I$(NETMAP_DIR)/sys
+LDFLAGS += -L$(NETMAP_DIR)/build-libnetmap
+endif
 LDFLAGS += -lnetmap
